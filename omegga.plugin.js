a88c58b0c4f504ad1c9336f515013f01343f5a8c
TheBluestBluester <95362358+TheBluestBluester@users.noreply.github.com>
2025-07-29 19:39:33 +0200

    Add files via upload

--- omegga.plugin.js
+++ omegga.plugin.js
@@ -70,6 +70,53 @@ module.exports = class Plugin {
 		
 	}
 	
+	async GetEntityLocation(entityName, entityID) {
+		try{
+		const reg = new RegExp(`BRSpherePhysicsStaticMeshComponent.+RelativeLocation =.+X=(?<X>.+),Y=(?<Y>.+),Z=(?<Z>.+).`);
+		
+		const data = await this.omegga.addWatcher(reg, {
+			exec: () =>
+			this.omegga.writeln(
+				`GetAll MeshComponent RelativeLocation Outer=${entityName}_${entityID}` // GetAll StaticMesh RelativeLocation Outer=${entityName}_${entityID}
+			),
+			first: 'index',
+			timeoutDelay: 500
+		});
+		
+		return [Number(data[0].groups.X), Number(data[0].groups.Y), Number(data[0].groups.Z)];
+		
+		}catch(e){console.log(e)}
+		
+	}
+	
+	async GetAllEntities(lastToFirst, toCheck) {
+		try{
+		const reg = new RegExp(`.+PersistentLevel.BP_Entity_(?<EntityName>.+)_C_(?<EntityID>\\d+).OwnerId = (?<OwnerID>\\d+)`);
+		
+		const data = await this.omegga.addWatcher(reg, {
+			exec: () =>
+			this.omegga.writeln(
+				`GetAll BREntityActor OwnerId`
+			),
+			first: 'index',
+			timeoutDelay: 200,
+			bundle: true
+		});
+		
+		let entityList = [];
+		for(let i in data) {
+			const groups = data[i].groups;
+			const ownerId = Number(groups.OwnerID);
+			if(toCheck >= 0 && ownerId != toCheck) continue;
+			entityList.push({name: groups.EntityName.replace(/Wheel_/, ""), trueName: groups.EntityName, id: groups.EntityID, ownerId: ownerId});
+		}
+		
+		if (lastToFirst) return entityList;
+		return entityList.reverse();
+		
+		}catch(e){console.log(e)}
+		
+	}
 	
 	async GetBrickOwnerTable() {
 		try{
@@ -122,6 +169,28 @@ module.exports = class Plugin {
 		
 	}
 	
+	fuzzyMatch = (toCheck, target) => {
+		
+		const item = toCheck.name.toLowerCase();
+		let score = 0;
+		
+		for(let c = 0; c < item.length; c++) {
+			
+			const itemChar = item.charAt(c);
+			const targetChar = target.charAt(c);
+			if(itemChar == targetChar) {
+				score++;
+			}
+			else {
+				break;
+			}
+			
+		}
+		
+		return score;
+		
+	}
+	
 	fuzzySearch = (list, target) => {
 		
 		const targetLowerCase = target.toLowerCase();
@@ -131,21 +200,7 @@ module.exports = class Plugin {
 		
 		for(let i in list) {
 			
-			const item = list[i].name.toLowerCase();
-			let score = 0;
-			
-			for(let c = 0; c < item.length; c++) {
-				
-				const itemChar = item.charAt(c);
-				const targetChar = targetLowerCase.charAt(c);
-				if(itemChar == targetChar) {
-					score++;
-				}
-				else {
-					break;
-				}
-				
-			}
+			const score = this.fuzzyMatch(list[i], targetLowerCase);
 			
 			if(score > highestScore) {
 				
@@ -210,12 +265,6 @@ module.exports = class Plugin {
 			for(let i in grids) {
 				
 				const grid = grids[i];
-				/*
-				if(grid.ownerId != ownerToCheck && ownerToCheck > 0) {
-					decrement++;
-					continue;
-				}
-				*/
 				const gridLoc = await this.GetGridLocation(grid.id);
 				const gridOwner = brickOwners[grid.ownerId].displayName;
 				
@@ -262,9 +311,87 @@ module.exports = class Plugin {
 			
 			this.omegga.writeln(`Chat.Command /TP "${name}" ${gridLoc[0]} ${gridLoc[1]} ${gridLoc[2]} 0`)
 			
+		})
+		.on('cmd:getents', async (name, ...args) => {
+			
+			if(!this.checkRoles(name)) {
+				this.omegga.whisper(name, clr.err + "You do not have the permission to use this command!");
+				return;
+			}
+			
+			if(name in timeoutList) {
+				this.omegga.whisper(name, clr.err + `You need to wait ${cooldownTime} second(s) before using the command again.`)
+				return;
+			}
+			
+			const invertOrder = args.includes("last");
+			let ownerToCheck = -1;
+			const argInd = args.findIndex((e) => e.includes("owner:"));
+			
+			this.omegga.whisper(name, clr.msg + "Fetching entities...");
+			
+			const brickOwners = await this.GetBrickOwnerTable();
+			if(brickOwners == null) {
+				
+				this.omegga.whisper(name, clr.err + "Failed to get brick owners.")
+				return;
+				
+			};
+			
+			if(argInd >= 0) {
+				
+				let target = args[argInd].replace(/owner:/, "");
+				if(target == "self") target = (await this.omegga.findPlayerByName(name)).displayName;
+				
+				ownerToCheck = this.fuzzySearch(brickOwners, target);
+				
+			}
+			
+			let entities = await this.GetAllEntities(invertOrder, ownerToCheck);
+			if(!entities) {
+				this.omegga.whisper(name, clr.err + "Failed to fetch entities.");
+				return;
+			}
+			
+			const filterInd = args.findIndex((e) => e.includes("type:"));
+			if(filterInd >= 0) {
+				
+				let target = args[filterInd].replace(/type:/, "").toLowerCase();
+				entities = entities.filter((e) => this.fuzzyMatch(e, target) >= target.length);
+				
+			}
+			
+			let entList = [];
+			
+			let decrement = 0;
+			for(let i in entities) {
+				
+				const entity = entities[i];
+				const entityName = "BP_Entity_" + entity.trueName + "_C";
+				const entityLoc = await this.GetEntityLocation(entityName, entity.id);
+				const entityOwner = brickOwners[entity.ownerId].displayName;
+				
+				let ind = Number(i) + 1;
+				if(invertOrder) ind = entities.length - Number(i);
+				
+				this.omegga.whisper(name, clr.msg + `(${ind}) <b>Name:</> ${entity.name} <b>Location:</> ${Math.floor(entityLoc[0])}, ${Math.floor(entityLoc[1])}, ${Math.floor(entityLoc[2])} <b>Owner:</> ${entityOwner}`);
+				
+				entList.push({id: entity.id, ownerId: entity.ownerId, loc: entityLoc});
+				
+			}
+			
+			if(entList.length == 0) {
+				this.omegga.whisper(name, clr.err + "No entities have been found!");
+				return;
+			}
+			
+			playerLists[name] = entList;
+			
+			timeoutList[name] = setTimeout(() => this.removeTimeout(name), cooldownTime * 1000);
+			
 		});
 
-		return { registeredCommands: ['getgrids', 'tptogrid'] };
+		return { registeredCommands: ['getgrids', 'tptogrid', 'getents'] };
 	}
 
 	async stop() {
